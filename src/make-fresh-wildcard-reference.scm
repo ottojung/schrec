5a671e32d05b2f457af089f94a74cf5304d8c15e
Otto Jung <otto.jung@kakao.com>
2021-11-28 00:31:16 +0100

    remove make-fresh-wildcard-reference

--- src/make-fresh-wildcard-reference.scm
+++
@@ -1,9 +0,0 @@
-
-%run guile
-
-%var make-fresh-wildcard-reference
-
-%use (make-fresh-reference) "./make-fresh-reference.scm"
-
-(define (make-fresh-wildcard-reference)
-  (make-fresh-reference '_ 'wildcard))
