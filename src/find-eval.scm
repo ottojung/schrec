8528397b06be82a99740610859a18d555a43cefa
Otto Jung <otto.jung@kakao.com>
2021-11-28 22:58:29 +0100

    find-eval: use efficient cycle detection

--- src/find-eval.scm
+++ src/find-eval.scm
@@ -3,21 +3,21 @@
 
 %var find-eval
 
-%use (make-hashset hashset-ref hashset-add!) "./euphrates/ihashset.scm"
-%use (node-id node-children) "./node.scm"
+%use (node-id node-children node-visited? set-node-visited?!) "./node.scm"
 %use (eval-node?) "./eval-node-huh.scm"
 
 ;; returns either the eval node, or #f
-;; TODO: consider using O(1) memory algorithm like the "turle and hare".
 ;; FIXME: abort if graph loops into itself!
 (define (find-eval graph)
-  (define H (make-hashset))
   (let loop ((parent #f) (graph graph))
-    (if (hashset-ref H (node-id graph)) #f ;; already visited
+    (if (node-visited? graph) #f
         (begin
-          (hashset-add! H (node-id graph))
-          (if (eval-node? graph) parent
-              (let cloop ((cs (node-children graph)))
-                (if (null? cs) #f
-                    (or (loop graph (car cs))
-                        (cloop (cdr cs))))))))))
+          (set-node-visited?! graph #t)
+          (let ((ret
+                 (if (eval-node? graph) parent
+                     (let cloop ((cs (node-children graph)))
+                       (if (null? cs) #f
+                           (or (loop graph (car cs))
+                               (cloop (cdr cs))))))))
+            (set-node-visited?! graph #f)
+            ret)))))
