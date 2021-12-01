e8ed8cc2188e8a9fb24d6070efc825b5f95b46f2
Otto Jung <otto.jung@kakao.com>
2021-12-01 20:04:40 +0100

    add reduce-loop

--- src/run-topdown-loop.scm
+++ src/run-topdown-loop.scm
@@ -3,19 +3,11 @@
 
 %var run-topdown-loop
 
-%use (reduce-topdown) "./reduce-topdown.scm"
+%use (reduce-loop) "./reduce-loop.scm"
 %use (find-bottommost-eval) "./find-bottommost-eval.scm"
-%use (node-children) "./node.scm"
-%use (get-eval-body) "./get-eval-body.scm"
-%use (get-eval-env) "./get-eval-env.scm"
 
 ;; NOTE: not semantically correct
 (define (run-topdown-loop g)
   (let ((eval-node (find-bottommost-eval g)))
     (and eval-node
-         (let ((env (get-eval-env eval-node)) ;; TODO(eval-syntax): check syntax
-               (body (get-eval-body eval-node)))
-           (let loop ((evaled? #f))
-             (if (reduce-topdown env body)
-                 (loop #t)
-                 evaled?))))))
+         (reduce-loop eval-node))))
