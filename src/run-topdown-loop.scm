d407827858b056b82113b03aea5b64446e89d573
Otto Jung <otto.jung@kakao.com>
2021-12-01 17:15:02 +0100

    use get-eval-body.scm

--- src/run-topdown-loop.scm
+++ src/run-topdown-loop.scm
@@ -6,13 +6,14 @@
 %use (reduce-topdown) "./reduce-topdown.scm"
 %use (find-bottommost-eval) "./find-bottommost-eval.scm"
 %use (node-children) "./node.scm"
+%use (get-eval-body) "./get-eval-body.scm"
 
 ;; NOTE: not semantically correct
 (define (run-topdown-loop g)
   (let ((eval-node (find-bottommost-eval g)))
     (and eval-node
          (let ((env (cadr (node-children eval-node))) ;; TODO(eval-syntax): check syntax
-               (body (caddr (node-children eval-node))))
+               (body (get-eval-body eval-node)))
            (let loop ((evaled? #f))
              (if (reduce-topdown env body)
                  (loop #t)
