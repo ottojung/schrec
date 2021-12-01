c1a330121d7022d520895fa32257c66410c83eb1
Otto Jung <otto.jung@kakao.com>
2021-12-02 00:03:11 +0100

    use list-find-first

--- src/run-topdown-ordered.scm
+++ src/run-topdown-ordered.scm
@@ -3,21 +3,15 @@
 
 %var run-topdown-ordered
 
+%use (list-find-first) "./euphrates/list-find-first.scm"
 %use (run-environment) "./run-environment.scm"
 %use (find-sorted-evals) "./find-sorted-evals.scm"
 %use (get-eval-env) "./get-eval-env.scm"
 %use (reduce-loop) "./reduce-loop.scm"
 
-%use (debug) "./euphrates/debug.scm"
-%use (graph->list) "./graph-to-list.scm"
-
 ;; NOTE: not semantically correct either
 (define (run-topdown-ordered graph)
   (let oloop ()
     (define evals (find-sorted-evals graph))
-    (when (let loop ((evals evals))
-            (if (null? evals) #f
-                (let ((cur (car evals)))
-                  (or (reduce-loop cur)
-                      (loop (cdr evals))))))
+    (when (list-find-first reduce-loop #f evals)
       (oloop))))
