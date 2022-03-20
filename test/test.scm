6539b0db3818697985fde12f782b5be89a38c731
Otto Jung <otto.jung@kakao.com>
2022-03-20 22:50:43 +0100

    test: add addition example

--- test/test.scm
+++ test/test.scm
@@ -258,13 +258,69 @@
 ;;               (num x))))
 ;;           ((s (s (s 0))) + ((s 0) + ((s (s 0)) + (s 0)))))))
 
+;; (define input
+;;   '(a d
+;;       (let (g x y)
+;;         (do (((+ -) g
+;;               (x + y)
+;;               (- x y)))
+;;             (1 + (2 + 3))))))
+
+;; (define input
+;;   '(let (g x y)
+;;      (do (((+ -) g
+;;            (x + y)
+;;            (- x y)))
+;;          (1 + (2 + 3)))))
+
+;; (define input
+;;   '(do ((let (g x y)
+;;           ((+ -) g
+;;            (x + y)
+;;            (- x y))))
+;;        (1 + (2 + 3))))
+
+;; (define input
+;;   '(do ((let (g x y)
+;;           ((+ -) g
+;;            (x + y)
+;;            (- x y))))
+;;        (1 + (2 + 3))))
+
 (define input
-  '(a d
-      (let (g x y)
-        (do (((+ -) g
-              (x + y)
-              (- x y)))
-            (1 + (2 + 3))))))
+  '(let ((body
+          ((s (s 0)) + (s (0 + (s ((s 0) + 0)))))))
+     (fork
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            ((s x) + 0) ;; match pattern
+            (s x)))     ;; replace pattern
+          body)
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            ((0 + 0) + x) ;; match pattern
+            (0 + x)))     ;; replace pattern
+          body)
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            (x + (0 + 0)) ;; match pattern
+            (x + 0)))     ;; replace pattern
+          body)
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            (s (0 + 0)) ;; match pattern
+            (s 0)))     ;; replace pattern
+          body)
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            (0 + (s x)) ;; match pattern
+            (s x)))     ;; replace pattern
+          body)
+      (do (((+ s 0)     ;; constants
+            g           ;; input
+            (x + (s y)) ;; match pattern
+            (s (x + y))))     ;; replace pattern
+          body))))
 
 (define graph
   (list->graph input))
