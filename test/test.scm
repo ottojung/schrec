ec9e90fb51c17e23b7cf53e3a62a13dbcc04aac7
Otto Jung <otto.jung@kakao.com>
2022-03-20 22:53:14 +0100

    test: add switch example

--- test/test.scm
+++ test/test.scm
@@ -287,40 +287,51 @@
 ;;            (- x y))))
 ;;        (1 + (2 + 3))))
 
+;; (define input
+;;   '(let ((body
+;;           ((s (s 0)) + (s (0 + (s ((s 0) + 0)))))))
+;;      (fork
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             ((s x) + 0) ;; match pattern
+;;             (s x)))     ;; replace pattern
+;;           body)
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             ((0 + 0) + x) ;; match pattern
+;;             (0 + x)))     ;; replace pattern
+;;           body)
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             (x + (0 + 0)) ;; match pattern
+;;             (x + 0)))     ;; replace pattern
+;;           body)
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             (s (0 + 0)) ;; match pattern
+;;             (s 0)))     ;; replace pattern
+;;           body)
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             (0 + (s x)) ;; match pattern
+;;             (s x)))     ;; replace pattern
+;;           body)
+;;       (do (((+ s 0)     ;; constants
+;;             g           ;; input
+;;             (x + (s y)) ;; match pattern
+;;             (s (x + y))))     ;; replace pattern
+;;           body))))
+
+;; const input test 1
 (define input
-  '(let ((body
-          ((s (s 0)) + (s (0 + (s ((s 0) + 0)))))))
-     (fork
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            ((s x) + 0) ;; match pattern
-            (s x)))     ;; replace pattern
-          body)
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            ((0 + 0) + x) ;; match pattern
-            (0 + x)))     ;; replace pattern
-          body)
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            (x + (0 + 0)) ;; match pattern
-            (x + 0)))     ;; replace pattern
-          body)
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            (s (0 + 0)) ;; match pattern
-            (s 0)))     ;; replace pattern
-          body)
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            (0 + (s x)) ;; match pattern
-            (s x)))     ;; replace pattern
-          body)
-      (do (((+ s 0)     ;; constants
-            g           ;; input
-            (x + (s y)) ;; match pattern
-            (s (x + y))))     ;; replace pattern
-          body))))
+  '(let ((switch (off)))
+     (do ((() g
+           (x)
+           (x (x)))
+          ((switch off on) switch
+           (off)
+           (on)))
+         (body))))
 
 (define graph
   (list->graph input))
