4edf26d1679240b8f96978fd56914458a8ae55cf
Otto Jung <otto.jung@vauplace.com>
2022-07-27 15:09:17 +0200

    update copyright years

--- src/initialize-const-variable-bang.scm
+++ src/initialize-const-variable-bang.scm
@@ -1,4 +1,4 @@
-;;;; Copyright (C) 2021  Otto Jung
+;;;; Copyright (C) 2021, 2022  Otto Jung
 ;;;;
 ;;;; This program is free software: you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
