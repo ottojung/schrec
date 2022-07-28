30e8cee48634925bdb1669d0693867b2f2ec20cd
Otto Jung <otto.jung@vauplace.com>
2022-07-28 22:58:23 +0200

    example: fix non-determinism-example-2

--- example/non-determinism-example-2.scm
+++ example/non-determinism-example-2.scm
@@ -1,8 +1,8 @@
 
 ;; non deterministism test 2
-(eval ((eval) g
-       (eval env body)
-       (body))
+(eval (((eval) g
+        (eval env body)
+        (body)))
       (let ((body (x)))
         (fork
          (eval (((x 1 2) g
--- test/expected-example-outputs/det/non-determinism-example-2.scm
+++ test/expected-example-outputs/det/non-determinism-example-2.scm
@@ -1,11 +1,22 @@
 Original:
 (let ((body.1 (x)))
-  ((eval ((eval) g (eval env body) (body))
+  ((eval (((eval) g (eval env body) (body)))
          (fork (eval (((x 1 2) g (x) (1))) body.1)
                (eval (((x 1 2) g (x) (2))) body.1)))))
 
 Step:
 (let ((body.1 (2)))
-  ((eval ((eval) g (eval env body) (body))
+  ((eval (((eval) g (eval env body) (body)))
          (fork (eval (((x 1 2) g (x) (1))) body.1)
                (eval (((x 1 2) g (x) (2))) body.1)))))
+
+Step:
+(let ((body.1 (2)))
+  ((eval (((eval) g (eval env body) (body)))
+         (fork (body.1)
+               (eval (((x 1 2) g (x) (2))) body.1)))))
+
+Step:
+(let ((body.1 (2)))
+  ((eval (((eval) g (eval env body) (body)))
+         (fork (body.1) (body.1)))))
