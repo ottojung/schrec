d194740f4ee988b481837c608b3e0192c24d5d57
Otto Jung <otto.jung@vauplace.com>
2022-08-22 23:27:01 +0200

    implement check-let-syntax

--- src/check-let-syntax.scm
+++ src/check-let-syntax.scm
@@ -16,5 +16,43 @@
 
 %var check-let-syntax
 
+%use (raisu) "./euphrates/raisu.scm"
+
+%use (get-let-bindings) "./get-let-bindings.scm"
+%use (get-let-body) "./get-let-body.scm"
+
 (define (check-let-syntax lst)
-  'TODO)
+  (unless (list? lst)
+    (raisu 'let-must-be-a-list lst))
+
+  (unless (< 0 (length lst))
+    (raisu 'let-should-not-be-empty lst))
+
+  (unless (< 1 (length lst))
+    (raisu 'let-does-not-even-have-bindings lst))
+
+  (unless (< 2 (length lst))
+    (raisu 'let-is-missing-a-body lst))
+
+  (let ((bindings (get-let-bindings lst)))
+    (for-each
+     (lambda (binding)
+       (unless (list? binding)
+         (raisu 'let-binding-must-be-a-list binding lst))
+
+       (unless (= 2 (length binding))
+         (raisu 'let-binding-must-be-of-length-2 binding lst))
+
+       (unless (symbol? (car binding))
+         (raisu 'let-binding-name-must-be-a-symbol binding lst))
+
+       (unless (or (symbol? (cadr binding))
+                   (list? (cadr binding))
+                   (number? (cadr binding)))
+         (raisu 'let-binding-value-must-be-of-simple-type
+                binding lst)))
+     bindings))
+
+  (let ((body (get-let-body lst)))
+    (unless (list? body)
+      (raisu 'let-body-must-be-a-list body))))
