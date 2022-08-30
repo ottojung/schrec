3a7b814cbdf30fc25da394f7655f755c5e8c64c2
Otto Jung <otto.jung@vauplace.com>
2022-08-30 21:46:00 +0200

    example: add failing test

---
+++ example/rewrite-to-multiple.scm
@@ -0,0 +1,6 @@
+
+;; FIXME: pass this test
+;; `xs` is bound to many nodes: to `1` and to `2` and to `3` and to `4`.
+;; Thus, the pattern should not match
+(eval g ((const g (xs) xs))
+      (1 2 3 4))
