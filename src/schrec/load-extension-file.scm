fe775c4aaaa7c174118376e320c918f5ebcc621a
Otto Jung <otto.jung@vauplace.com>
2023-05-18 23:09:56 -0700

    add load-extension-file.scm

---
+++ src/schrec/load-extension-file.scm
@@ -0,0 +1,30 @@
+;;;; Copyright (C) 2023 Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(cond-expand
+ (guile
+  (define-module (schrec load-extension-file)
+    :export (load-extension-file)
+    :use-module ((euphrates dynamic-load) :select (dynamic-load))
+    :use-module ((euphrates file-or-directory-exists-q) :select (file-or-directory-exists?))
+    :use-module ((euphrates raisu) :select (raisu))
+    :use-module ((schrec load-extension) :select (load-extension))
+    )))
+
+(define (load-extension-file filepath)
+  (unless (file-or-directory-exists? filepath)
+    (raisu 'extension-file-does-not-exist filepath))
+
+  (let ((manifest (dynamic-load filepath)))
+    (load-extension manifest)))
